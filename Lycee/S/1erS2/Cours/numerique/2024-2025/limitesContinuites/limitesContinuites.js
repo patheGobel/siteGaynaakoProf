5eaeaa03a8024c326d6bb60e9f5f81fea17e2eae
patheGobel <pathegba@gmail.com>
2026-08-31 17:07:12 +0000

    Retouche de l'exemple et exercice

--- Lycee/S/1erS2/Cours/numerique/2024-2025/limitesContinuites/limitesContinuites.js
+++ Lycee/S/1erS2/Cours/numerique/2024-2025/limitesContinuites/limitesContinuites.js
@@ -3,15 +3,6 @@ function toggleExemple(element) {
     const contenu = element.nextElementSibling;
 
     contenu.classList.toggle("visible");
-
-    element.classList.toggle("ouvert");
-
-}
-function toggleExemple(element) {
-
-    const contenu = element.nextElementSibling;
-
-    contenu.classList.toggle("visible");
     element.classList.toggle("ouvert");
 }
 
@@ -23,10 +14,30 @@ function toggleSolution(element) {
     contenu.classList.toggle("visible");
     element.classList.toggle("ouvert");
 }
+
+/* =========================================================
+   AFFICHER / MASQUER UN EXERCICE
+   ========================================================= */
+
 function toggleExercice(element) {
 
     const contenu = element.nextElementSibling;
 
     contenu.classList.toggle("visible");
     element.classList.toggle("ouvert");
+
+}
+
+
+/* =========================================================
+   AFFICHER / MASQUER UNE CORRECTION
+   ========================================================= */
+
+function toggleSolution(element) {
+
+    const contenu = element.nextElementSibling;
+
+    contenu.classList.toggle("visible");
+    element.classList.toggle("ouvert");
+
 }
