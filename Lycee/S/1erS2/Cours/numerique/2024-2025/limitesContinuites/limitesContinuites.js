7b9501cb4f429b7681f42de8aba8eb88d9059eb8
patheGobel <pathegba@gmail.com>
2026-08-31 13:01:49 +0000

    I. Notion de limite. Limites à l'infini de :  \mapsto ax^n$ avec  \in \mathbb{R}\setminus\{0\}$ et  \in \mathbb{N}\setminus\{0\}$. Exemple 2

--- Lycee/S/1erS2/Cours/numerique/2024-2025/limitesContinuites/limitesContinuites.js
+++ Lycee/S/1erS2/Cours/numerique/2024-2025/limitesContinuites/limitesContinuites.js
@@ -7,3 +7,19 @@ function toggleExemple(element) {
     element.classList.toggle("ouvert");
 
 }
+function toggleExemple(element) {
+
+    const contenu = element.nextElementSibling;
+
+    contenu.classList.toggle("visible");
+    element.classList.toggle("ouvert");
+}
+
+
+function toggleSolution(element) {
+
+    const contenu = element.nextElementSibling;
+
+    contenu.classList.toggle("visible");
+    element.classList.toggle("ouvert");
+}
